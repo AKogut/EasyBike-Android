1e675857724c421cd14b88f7df73688294a0b9c5
Andriy Kogut <32357177+AKogut@users.noreply.github.com>
2018-08-12 22:44:40 +0300

    In mac

--- app/src/main/java/ua/in/easybike/MainActivity.java
+++ app/src/main/java/ua/in/easybike/MainActivity.java
@@ -1,9 +1,7 @@
 package ua.in.easybike;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
